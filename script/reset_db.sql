c5effe4a191f3c82e4fe192cede57192f847523e
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-04 16:16:00 +0300

    Fix email regex

--- script/reset_db.sql
+++ script/reset_db.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
   first_name VARCHAR(64) NOT NULL CHECK (first_name != ''),
   last_name VARCHAR(64) NOT NULL CHECK (last_name != ''),
   email VARCHAR(128) NOT NULL UNIQUE CONSTRAINT "email must be valid" 
-  CHECK (email ~ '[A-Za-z0-9]{3,32}@[a-z]{2,8}\.[a-z]{2,8}'),
+  CHECK (email ~ '.{3,32}@[a-z]{2,8}\.[a-z]{2,8}'),
   birthday DATE CHECK (birthday BETWEEN '1900-01-01' AND current_date),
   is_male BOOLEAN,
   current_balance NUMERIC(10,2) NOT NULL DEFAULT 0 CONSTRAINT "current_balance cant be negative" CHECK (current_balance >= 0),
