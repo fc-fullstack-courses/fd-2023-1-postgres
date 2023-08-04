005c136f23f3e9c8b4365980e7c1c3224b0537ef
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-04 16:36:46 +0300

    Add separate pg db connection file

--- script/index.js
+++ script/index.js
@@ -3,13 +3,7 @@ const fs = require('fs/promises');
 const { loadUsers } = require('./api/index');
 const { generateUsersSql } = require('./utils/users');
 
-const config = {
-  user: 'postgres', // default process.env.PGUSER || process.env.USER
-  password: 'postgres', //default process.env.PGPASSWORD
-  host: 'localhost', // default process.env.PGHOST
-  port: 5432, // default process.env.PGPORT
-  database: 'fd_2023_1_first_pg_db_1', // default process.env.PGDATABASE || user
-}
+const config = require('./configs/db.json');
 
 const client = new Client(config);
 
