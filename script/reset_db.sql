ce276224e4eafa9b44c118eb653f8e2da6d74123
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-08 22:49:10 +0300

    Update reset_db script

--- script/reset_db.sql
+++ script/reset_db.sql
@@ -1,4 +1,9 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS products_to_orders CASCADE;
+DROP TABLE IF EXISTS ratings CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
 --
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
@@ -16,3 +21,46 @@ CREATE TABLE IF NOT EXISTS users (
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
+--
+CREATE TABLE IF NOT EXISTS orders (
+  id SERIAL PRIMARY KEY,
+  user_id integer NOT NULL REFERENCES users,
+  created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+--
+CREATE TABLE IF NOT EXISTS products (
+  id SERIAL PRIMARY KEY,
+  name varchar(300) NOT NULL,
+  category varchar(100),
+  description text,
+  manufacturer text,
+  price numeric(9,2) NOT NULL CHECK (price > 0),
+  quantity integer NOT NULL CHECK (quantity >= 0)
+);
+--
+CREATE TABLE IF NOT EXISTS products_to_orders (
+  product_id integer REFERENCES products,
+  order_id integer REFERENCES orders,
+  quantity integer NOT NULL CHECK (quantity > 0),
+  PRIMARY KEY (product_id, order_id)
+);
+--
+CREATE TABLE IF NOT EXISTS reviews (
+  id SERIAL PRIMARY KEY,
+  product_id int NOT NULL REFERENCES products,
+  user_id int NOT NULL REFERENCES users,
+  -- rating_id int NOT NULL UNIQUE REFERENCES ratings,
+  body text,
+  UNIQUE (product_id, user_id)
+);
+--
+CREATE TABLE IF NOT EXISTS ratings (
+  id SERIAL PRIMARY KEY,
+  review_id int NOT NULL UNIQUE 
+  REFERENCES reviews ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED,
+  rating numeric (2,1)
+);
+--
+ALTER TABLE reviews
+ADD COLUMN rating_id int NOT NULL UNIQUE 
+REFERENCES ratings ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
