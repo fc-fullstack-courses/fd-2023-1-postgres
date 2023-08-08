aa9d2fe639c10bf8d6a090ec4f12c2cc1cbb95ce
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-08 16:54:29 +0300

    Refactor foreign key constraint

--- sql/relations.sql
+++ sql/relations.sql
@@ -21,7 +21,7 @@
 DROP TABLE IF EXISTS orders;
 CREATE TABLE IF NOT EXISTS orders (
   id SERIAL PRIMARY KEY,
-  user_id integer NOT NULL REFERENCES users (id),
+  user_id integer NOT NULL REFERENCES users,
   created_at timestamp NOT NULL DEFAULT current_timestamp
 );
 -- оформление заказа
@@ -50,8 +50,8 @@ INSERT INTO products
 ('UAV', 'hobby', 'It flies and makes boom', 30000, 15);
 -- @block products_to_orders (n : m)
 CREATE TABLE IF NOT EXISTS products_to_orders (
-  product_id integer REFERENCES products (id),
-  order_id integer REFERENCES orders (id),
+  product_id integer REFERENCES products,
+  order_id integer REFERENCES orders,
   quantity integer NOT NULL CHECK (quantity > 0),
   PRIMARY KEY (product_id, order_id)
 );
@@ -65,9 +65,9 @@ INSERT INTO products_to_orders
 -- @block reviews table
 CREATE TABLE IF NOT EXISTS reviews (
   id SERIAL PRIMARY KEY,
-  product_id int NOT NULL REFERENCES products (id),
-  user_id int NOT NULL REFERENCES users (id),
-  -- rating_id int NOT NULL UNIQUE REFERENCES ratings (id),
+  product_id int NOT NULL REFERENCES products,
+  user_id int NOT NULL REFERENCES users,
+  -- rating_id int NOT NULL UNIQUE REFERENCES ratings,
   body text,
   UNIQUE (product_id, user_id)
 );
@@ -75,13 +75,13 @@ CREATE TABLE IF NOT EXISTS reviews (
 CREATE TABLE IF NOT EXISTS ratings (
   id SERIAL PRIMARY KEY,
   review_id int NOT NULL UNIQUE 
-  REFERENCES reviews (id) DEFERRABLE INITIALLY DEFERRED,
+  REFERENCES reviews DEFERRABLE INITIALLY DEFERRED,
   rating numeric (2,1)
 );
 --
 ALTER TABLE reviews
 ADD COLUMN rating_id int NOT NULL UNIQUE 
-REFERENCES ratings (id) DEFERRABLE INITIALLY DEFERRED;
+REFERENCES ratings DEFERRABLE INITIALLY DEFERRED;
 -- @block rating and review insert
 BEGIN;
 
