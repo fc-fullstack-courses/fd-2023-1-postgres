9bd982bf4640e3d82a59691abd79b52ca197c15d
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-08 23:48:18 +0300

    Add reviews and ratings generation

--- script/index.js
+++ script/index.js
@@ -4,6 +4,7 @@ const { loadUsers } = require('./api/index');
 const { orders: { chanceToMakeOrder } } = require('./configs/generation.json');
 const { client, User, Product, Order } = require('./models');
 const { generateProductsToOrders } = require('./utils/orders');
+const { generateReviewsMap, generateReviews, generateRatings } = require('./utils/reviewsRatings');
 
 async function generateData() {
   const resetDbString = await fs.readFile(`./script/reset_db.sql`, { encoding: 'utf-8' });
@@ -33,6 +34,22 @@ async function generateData() {
     ${generateProductsToOrders({ orders, products })}
     RETURNING *
     ;
+  `);
+
+  const reviewsMap = generateReviewsMap(orders, productsToOrdersResult);
+
+  await client.query(`
+  BEGIN;
+  
+  INSERT INTO reviews
+  (user_id, product_id, rating_id, body)
+  VALUES ${generateReviews(reviewsMap)};
+  
+  INSERT INTO ratings
+  (review_id, rating)
+  VALUES ${generateRatings(reviewsMap)};
+  
+  COMMIT;
   `);
 
   await client.end();
