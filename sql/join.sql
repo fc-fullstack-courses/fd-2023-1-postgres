af889c03506f87bf28191243c42575f366c74b05
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-09 15:58:27 +0300

    Add JOIN example

--- sql/join.sql
+++ sql/join.sql
@@ -1,3 +1,7 @@
 -- @block базовый приемер. Получить емейлы всех ползователей с заказами
 SELECT email, orders.id FROM users, orders
 WHERE user_id = users.id;
+-- @block JOIN example
+SELECT email, orders.id
+FROM users
+JOIN orders ON user_id = users.id;
