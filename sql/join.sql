6d390af5c5512405907b010d59e96a7c778d0303
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-09 16:45:07 +0300

    Add JOIN variations example

--- sql/join.sql
+++ sql/join.sql
@@ -4,7 +4,7 @@ WHERE user_id = users.id;
 -- @block JOIN example
 SELECT email, orders.id
 FROM users
-JOIN orders ON user_id = users.id;
+INNER JOIN orders ON user_id = users.id;
 -- @block JOIN example 2
 SELECT email, orders.id
 FROM orders
@@ -18,3 +18,24 @@ JOIN ratings ON rating_id = ratings.id;
 SELECT name, price, products_to_orders.quantity, orders.id FROM orders
 JOIN products_to_orders ON order_id = orders.id
 JOIN products ON product_id = products.id;
+-- @block CROSS JOIN (декартово произведение)
+SELECT * FROM orders
+CROSS JOIN users;
+-- @block LEFT JOIN - берет все записи из левой таблицы и добавляет к ним значения из правой таблицы по условию
+SELECT * FROM
+users -- левая таблица
+LEFT JOIN
+orders -- правая таблица
+ON user_id = users.id;
+-- @block RIGHT JOIN - берет все записи из правой таблицы и добавляет к ним значения из левой таблицы по условию
+SELECT users.*, orders.* FROM
+orders
+RIGHT JOIN
+users
+ON user_id = users.id;
+-- @block FULL JOIN - результаты левого и правого джойна для соединяемых таблиц
+SELECT users.*, orders.* FROM
+orders
+FULL JOIN
+users
+ON user_id = users.id;
