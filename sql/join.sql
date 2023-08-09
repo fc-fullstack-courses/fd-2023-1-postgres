1d8716d2009b67691a048f8777747400562f403b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-09 16:08:40 +0300

    Add multiple table JOIN example

--- sql/join.sql
+++ sql/join.sql
@@ -5,3 +5,12 @@ WHERE user_id = users.id;
 SELECT email, orders.id
 FROM users
 JOIN orders ON user_id = users.id;
+-- @block JOIN example 2
+SELECT email, orders.id
+FROM orders
+JOIN users ON user_id = users.id;
+-- @block все продукты с отзывами и оценками
+SELECT name, body, rating 
+FROM products
+JOIN reviews ON product_id = products.id
+JOIN ratings ON rating_id = ratings.id;
