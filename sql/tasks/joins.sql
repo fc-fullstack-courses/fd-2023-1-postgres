1dad839499ed417e4531d14f9b1b950e15644802
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-10 17:00:29 +0300

    Add some task solutions

--- sql/tasks/joins.sql
+++ sql/tasks/joins.sql
@@ -3,7 +3,7 @@
 
   -- количество уникальных товаров определенной категории в определенном заказе
 
-  -- количество отзывов о товаре с оценкой выше 4
+  -- количество отзывов о конкретном товаре с оценкой выше 4
 
   -- стоимость определенного заказа
 
@@ -13,3 +13,32 @@
 
   --** отобразить все заказы, итоговая стоимость которых выше средней стоимости заказа (через подзапросы)
 */
+
+-- @block все товары в определенном заказе
+SELECT name, manufacturer, price, pto.quantity
+FROM products p
+JOIN products_to_orders pto ON product_id = p.id
+WHERE order_id = 4;
+-- @block количество уникальных товаров определенной категории в определенном заказе
+SELECT count(*) "уникальные товары из группы хобби"
+FROM products p
+JOIN products_to_orders pto ON product_id = p.id
+WHERE order_id = 1 AND category = 'hobby';
+-- @block количество отзывов о конкретном товаре с оценкой выше 4
+SELECT name, count(product_id)
+FROM reviews rev
+JOIN ratings rat ON review_id = rev.id
+JOIN products p ON product_id = p.id
+WHERE rating > 4 AND product_id = 192
+GROUP BY p.id;
+-- @block стоимость определенного заказа
+SELECT sum( price * pto.quantity)
+FROM products_to_orders pto
+JOIN products p ON p.id = product_id
+WHERE order_id = 115;
+--
+SELECT  price, pto.quantity
+FROM products_to_orders pto
+JOIN products p ON p.id = product_id
+WHERE order_id = 115;
+-- 93038 + 102220 + 140065
