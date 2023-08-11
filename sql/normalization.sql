87e3b3e8f3d3b90950bbf12f83b263ad0799e33a
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-11 16:01:58 +0300

    Add 1 NF example

--- sql/normalization.sql
+++ sql/normalization.sql
@@ -10,3 +10,14 @@
     - Доменно-ключевая
     - 6
 */
+-- @block 1 NF
+-- отношение не в 1 NF
+CREATE TABLE workers (
+  id INT,
+  name TEXT
+);
+-- отношение в 1 NF
+CREATE TABLE workers (
+  id INT PRIMARY KEY,
+  name TEXT
+);
