9a4cab4a6b00bdb4cdb4552993b9eef9278b2371
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-11 16:15:12 +0300

    Add 2 NF example

--- sql/normalization.sql
+++ sql/normalization.sql
@@ -21,3 +21,22 @@ CREATE TABLE workers (
   id INT PRIMARY KEY,
   name TEXT
 );
+-- @block 2 NF
+-- отношение не в 2 NF
+CREATE TABLE teachers_to_course (
+  teacher_id INT,
+  course TEXT,
+  teachers_birthday DATE,
+  PRIMARY KEY (teacher_id, course)
+);
+--  отношение в 2 NF
+CREATE TABLE teachers (
+    teacher_id INT PRIMARY KEY,
+    teachers_birthday DATE
+);
+--
+CREATE TABLE teachers_to_course (
+  teacher_id INT REFERENCES teachers,
+  course TEXT,
+  PRIMARY KEY (teacher_id, course)
+);
