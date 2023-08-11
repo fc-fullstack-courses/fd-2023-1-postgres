8da5e1373395e0b9d9d18f60ee05b9d2573c2d19
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-11 16:34:48 +0300

    Add 3 NF example

--- sql/normalization.sql
+++ sql/normalization.sql
@@ -40,3 +40,44 @@ CREATE TABLE teachers_to_course (
   course TEXT,
   PRIMARY KEY (teacher_id, course)
 );
+-- @block 3 NF
+/*
+  потенциальный ключ - столбец / группа столбцов по которой можно найти запись (огр. UNIQUE, PRIMARY KEY)
+
+  транзитивная функциональная зависимость - зависимость не только от ключа но и от промежуточного аттрибута
+
+  хорошо:
+    id > attr1 
+    id > attr2 
+
+  плохо:
+    id > attr1 > attr2
+    id > attr2
+*/
+-- отношение не в 3 NF
+CREATE TABLE workers (
+  id INT PRIMARY KEY,
+  name TEXT,
+  department TEXT,
+  department_phone_number TEXT
+);
+--
+INSERT INTO workers
+(name, department, department_phone_number) VALUES
+('W1', 'IT', '123-456-789'),
+('W2', 'IT', '123-456-789'),
+('W3', 'HR', '124-555-777'),
+('W4', 'DevOps', '987-321-654'),
+('W5', 'HR', '987-321-654');
+-- отношение в 3 NF
+CREATE TABLE workers (
+  id INT PRIMARY KEY,
+  department_id INT,
+  name TEXT
+);
+--
+CREATE TABLE departments (
+  id INT PRIMARY KEY,
+  name TEXT,
+  department_phone_number TEXT
+);
